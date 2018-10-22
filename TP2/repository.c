7b838f730966834369255ea540e7c0f735a772b9
juandahl <juandahl20@gmail.com>
2018-10-22 18:27:51 +0200

    changes in repository logic

---
+++ TP2/repository.c
@@ -0,0 +1,82 @@
+// gcc -o remplissage remplissage.c -lsqlite3
+#include <sqlite3.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+// retourne un entier aléatoire entre 0 et n
+int int_rand(int n){
+  return (rand()%n);
+}
+
+// return un float aléatoire entre 0 et f
+float float_rand(float f){
+  return (((float)rand()/(float)(RAND_MAX)) * f);
+}
+
+int readDB(){
+  sqlite3 *db;
+  char *err_msg;
+  sqlite3_stmt *stmt;
+  char req[255];
+  int rc, s;
+
+  // initialisation de la graine pour les nbs aléatoires
+  srand((unsigned int)time(NULL));
+
+  // ouverture de la base de données
+  rc=sqlite3_open("mabdd.db", &db);
+
+  // lecture dans la base
+  
+  char reqSelect[255] = "SELECT * FROM Mesure;";
+  
+  // requête SQL select
+  sprintf(req,"SELECT * FROM Mesure;");
+
+  // préparation de la requête
+  rc=sqlite3_prepare_v2(db, req, -1, &stmt, 0);
+  // parcours des enregistrements
+  while(1){
+    // lecture de l'enregistrement suivant
+    s=sqlite3_step(stmt);
+    // enregistrement existant
+    if (s==SQLITE_ROW){
+      // récupération de la valeur de la colonne valeur
+      const unsigned char *val=sqlite3_column_text(stmt, 2);
+      //add to web graphic
+    }
+    // parcours terminé
+    else if (s==SQLITE_DONE)
+      break;
+  }
+  // fermeture de la base de données
+  sqlite3_close(db);
+  return 0;
+
+}
+
+void updateDB(int id,int val){
+  sqlite3 *db;
+  char *err_msg;
+  sqlite3_stmt *stmt;
+  char req[255];
+  int rc, s;
+
+  // initialisation de la graine pour les nbs aléatoires
+  srand((unsigned int)time(NULL));
+
+  // ouverture de la base de données
+  rc=sqlite3_open("mabdd.db", &db);
+
+  // insertion de données dans la base
+  
+      
+  // requête SQL insert
+  
+  sprintf(req,"%s%d%s%d%s","INSERT INTO Mesure(valeur, id_capteur) VALUES(", val,",", id, ");");
+  printf("sql=|%s|\n",req);
+  // exécution de la requête
+  rc = sqlite3_exec(db, req, 0, 0, &err_msg);
+}
--- TP2/server3.c
+++ TP2/server3.c
@@ -1,97 +1,21 @@
-// to run: gcc -o server2 server2.c -pthread
+// to compile: gcc -o server2 server2.c -pthread
+// to run 
 #include<stdio.h>
 #include<string.h>    //strlen
 #include<sys/socket.h>
 #include<arpa/inet.h> //inet_addr
 #include<unistd.h>    //write
 #include <stdlib.h>
-#include<pthread.h>
-#include <sqlite3.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <time.h>
+#include "repository.c"
 
 //buffer declaration
 char buffer [256];
 
 
-// retourne un entier aléatoire entre 0 et n
-int int_rand(int n){
-  return (rand()%n);
-}
-
-// return un float aléatoire entre 0 et f
-float float_rand(float f){
-  return (((float)rand()/(float)(RAND_MAX)) * f);
-}
-
-int readDB(){
-  sqlite3 *db;
-  char *err_msg;
-  sqlite3_stmt *stmt;
-  char req[255];
-  int rc, s;
-
-  // initialisation de la graine pour les nbs aléatoires
-  srand((unsigned int)time(NULL));
-
-  // ouverture de la base de données
-  rc=sqlite3_open("mabdd.db", &db);
-
-  // lecture dans la base
-  
-  char reqSelect[255] = "SELECT * FROM Mesure;";
-  
-  // requête SQL select
-  sprintf(req,"SELECT * FROM Mesure;");
-
-  // préparation de la requête
-  rc=sqlite3_prepare_v2(db, req, -1, &stmt, 0);
-  // parcours des enregistrements
-  while(1){
-    // lecture de l'enregistrement suivant
-    s=sqlite3_step(stmt);
-    // enregistrement existant
-    if (s==SQLITE_ROW){
-      // récupération de la valeur de la colonne valeur
-      const unsigned char *val=sqlite3_column_text(stmt, 2);
-      //add to web graphic
-    }
-    // parcours terminé
-    else if (s==SQLITE_DONE)
-      break;
-  }
-  // fermeture de la base de données
-  sqlite3_close(db);
-  return 0;
-
-}
-
-void updateDB(int val,int id){
-  sqlite3 *db;
-  char *err_msg;
-  sqlite3_stmt *stmt;
-  char req[255];
-  int rc, s;
-
-  // initialisation de la graine pour les nbs aléatoires
-  srand((unsigned int)time(NULL));
-
-  // ouverture de la base de données
-  rc=sqlite3_open("mabdd.db", &db);
-
-  // insertion de données dans la base
-  
-      
-  // requête SQL insert
-  
-  sprintf(req,"INSERT INTO Mesure(valeur, id_capteur) VALUES(val, id);");
-  printf("sql=|%s|\n",req);
-  // exécution de la requête
-  rc = sqlite3_exec(db, req, 0, 0, &err_msg);
-}
-
-
-void *server_handler(void *arg){
+void *insert_DB(void *arg){
 	//variables declaration
 	char buffer[256];
 	char val[256];
@@ -134,7 +58,7 @@ int main(int argc , char *argv[])
 
 
 	//declaration and initialization
-        struct sockaddr_in server_ad, client_ad;
+    struct sockaddr_in server_ad, client_ad;
 
 
 	//Create socket
@@ -147,7 +71,7 @@ int main(int argc , char *argv[])
 
 
 	//bind
-    	bind(socket_fd, (struct sockaddr*)&server_ad, sizeof(server_ad)); 
+    bind(socket_fd, (struct sockaddr*)&server_ad, sizeof(server_ad)); 
 	int clilen = sizeof(struct sockaddr_in);
   
   	//listen
@@ -164,8 +88,8 @@ int main(int argc , char *argv[])
 		*new_sock = client_socket_fd;
 
 		//for each client request creates a thread and assign the client request to it to process
-	        //so the main thread can entertain next request
-		pthread_create(&server_thread, NULL, server_handler, (void*) new_sock);
+        //so the main thread can entertain next request
+		pthread_create(&server_thread, NULL, insert_DB, (void*) new_sock);
 				
 	}
 	return 0;	
