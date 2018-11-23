330b6e8494c614efcdcc14b97eb64f7673fa46ec
juandahl <juandahl20@gmail.com>
2018-11-24 00:11:10 +0100

    Home with api working

--- server/services.c
+++ server/services.c
@@ -4,7 +4,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <curl/curl.h>
+#include <time.h>
+
 #include "repository.c"
+
+#define GMT (+1)
+
+//API response
+struct string {
+  char *ptr;
+  size_t len;
+};
 
 
 //SEGURAMENTE SE BORRA. VER
@@ -34,39 +44,113 @@ void parseEntryDB(int newSocket, char buffer[256]){
 
 void parseWeather(char *buff)
 {
-	strcat(buff,"</td>\n");
-    strcat(buff,"<td>");
+	//get current day and convert it to string
+	char *week[7] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+	time_t rawtime;
+	struct tm *info;
+
+	time(&rawtime);
+	/* Get GMT time */
+	info = gmtime(&rawtime);
+
+	int day = info->tm_wday;
+
+	//call weather API
+	int *response[5];
+	getWheather(response);
+	char strNumber[2];
+
+	//finally create code html
+	strcat(buff,"<div class=\"w3-xlarge w3-display-bottomleft w3-padding\">PARIS ");
+	sprintf(strNumber, "%d", response[0]);
+	strcat(buff,strNumber);
+	strcat(buff,"&deg; C</div>\n");	
+	strcat(buff,"</div>\n");
+	strcat(buff,"<div class=\"w3-cell-row\">\n");
+
+	//create the code html for next days
+	for (int i = 1; i < 5; i++)
+	{
+		strcat(buff,"<div class=\"w3-third w3-center\">\n");
+		strcat(buff,"<h3>");
+		strcat(buff, week[(day+i)%7]);
+		strcat(buff,"</h3>\n");
+		strcat(buff,"<p>PARIS ");
+		sprintf(strNumber, "%d", response[i]);
+		strcat(buff, strNumber);
+		strcat(buff,"&deg; C</p>\n");		
+		strcat(buff,"</div>\n");
+	}
+	strcat(buff,"</div>\n");
 
 }
 
-//Implementation apiREST get
-int getWheather(char *response){
-  CURL *curl;
-  CURLcode res;
-  char *s;
- 
-  curl = curl_easy_init();
-  if(curl) {
-	//simple request commented
-	//curl_easy_setopt(curl, CURLOPT_URL, "https://api.openweathermap.org/data/2.5/weather?q=Paris,fr&appid=0e1b6106f4fb2445ef36c66b4dddbeda");
-    curl_easy_setopt(curl, CURLOPT_URL, "https://api.openweathermap.org/data/2.5/forecast?q=Paris&mode=json&units=metric&cnt=5&appid=0e1b6106f4fb2445ef36c66b4dddbeda");
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
 
-    // Perform the request, res will get the return code  
-    res = curl_easy_perform(curl);
-
-    // Check for errors  
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
-    // always cleanup  
-    curl_easy_cleanup(curl);
+void init_string(struct string *s) {
+  s->len = 0;
+  s->ptr = malloc(s->len+1);
+  if (s->ptr == NULL) {
+    fprintf(stderr, "malloc() failed\n");
+    exit(EXIT_FAILURE);
   }
-	printf("%s\n", s);
+  s->ptr[0] = '\0';
+}
 
-  return 0;
+size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s)
+{
+  size_t new_len = s->len + size*nmemb;
+  s->ptr = realloc(s->ptr, new_len+1);
+  if (s->ptr == NULL) {
+    fprintf(stderr, "realloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+  memcpy(s->ptr+s->len, ptr, size*nmemb);
+  s->ptr[new_len] = '\0';
+  s->len = new_len;
 
+  return size*nmemb;
+}
+
+//Implementation apiREST get
+int getWheather(int *temp[5]){
+	CURL *curl;
+	CURLcode res;
+
+	curl = curl_easy_init();
+	if(curl) {
+	struct string s;
+	init_string(&s);
+
+	curl_easy_setopt(curl, CURLOPT_URL, "https://api.openweathermap.org/data/2.5/forecast?q=Paris&mode=json&units=metric&cnt=5&appid=0e1b6106f4fb2445ef36c66b4dddbeda");
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+	res = curl_easy_perform(curl);
+	int j = 0;
+	for (int i=0; i< s.len; i++)
+	{
+		if ( strncmp(s.ptr+i, "\"temp\"", 6) == 0){
+			char aux[2];
+			if (isdigit((s.ptr+i)[8]))
+			{
+				aux[0] = (s.ptr+i)[7];
+				aux[1] = (s.ptr+i)[8];
+			}
+			else
+			{
+				aux[0] = '0';
+				aux[1] = (s.ptr+i)[7];
+			}
+			int number = atoi(aux);
+			temp[j] = number;
+			j++;
+		} 
+	}
+	free(s.ptr);
+
+	/* always cleanup */
+	curl_easy_cleanup(curl);
+	}
+	return 0;
 }
 
 //HTML METHODS
@@ -127,10 +211,9 @@ void completePageIndex(FILE *fp, char *buff)
 	//Load the html in buff string
 	char line[256];
 	while (fgets(line, sizeof(line), fp))  {
-		if(strncmp(line, "	                    <div class=\"w3-cell-row\">", 40) == 0) //upadte
+		if(strncmp(line, "<div class=\"w3-xlarge w3-display-bottomleft w3-padding\">PARIS 20&deg; C</div>", 10) == 0) 
 		{
-			strcat(buff, (char*)line);
-			//parseWeather(buff);
+			parseWeather(buff);
 		}
 		else
 			strcat(buff, (char*)line);
